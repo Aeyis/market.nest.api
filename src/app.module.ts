6267af828e56ba75dd2ce1adf3f754f8dfa6d547
Rafael Solis Ramos <rafaelsolioramoso@gmail.com>
2026-04-13 15:38:37 +0200

    modif app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MinimarketController } from './controllers/minimarket/minimarket.controller';
 import { MinimarketService } from './services/minimarket/minimarket.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ProductEntity} from "./entities/product.entity";
+import {CategoryEntity} from "./entities/category.entity";
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'mini-market',
+      username: 'postgres',
+      password: 'postgres',
+      synchronize: true,
+      logging: true,
+      entities: [ProductEntity, CategoryEntity],
+    }),
+      TypeOrmModule.forFeature([ProductEntity, CategoryEntity]),
+  ],
   controllers: [AppController, MinimarketController],
   providers: [AppService, MinimarketService],
 })
