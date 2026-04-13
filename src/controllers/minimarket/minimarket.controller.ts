68f4949491d8ddd59eb8c4acfd37870dc4d3c667
Rafael Solis Ramos <rafaelsolioramoso@gmail.com>
2026-04-13 16:16:25 +0200

    feat: add minimarket controller routes

--- src/controllers/minimarket/minimarket.controller.ts
+++ src/controllers/minimarket/minimarket.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
+import {MinimarketService} from "../../services/minimarket/minimarket.service";
+import {ProductEntity} from "../../entities/product.entity";
+import {CategoryEntity} from "../../entities/category.entity";
 
 @Controller('minimarket')
-export class MinimarketController {}
+export class MinimarketController {
+    constructor(private readonly minimarketService: MinimarketService) {}
+
+    @Get('products')
+    async getProducts(): Promise<ProductEntity[]> {
+        return await this.minimarketService.getProducts();
+    }
+
+    @Get('products/:id')
+    async getProduct(@Param('id') id: string): Promise<ProductEntity | null>{
+        return await this.minimarketService.getProductById(+id);
+    }
+
+    @Post('products')
+    async createProduct(@Body() body: ProductEntity): Promise<ProductEntity> {
+        return await this.minimarketService.createProduct(body);
+    }
+
+    @Patch('products/:id')
+    async updateProduct (@Param('id') id: string, @Body() body: ProductEntity): Promise<ProductEntity> {
+        return await this.minimarketService.updateProduct(+id, body);
+    }
+
+    @Delete('products/:id')
+    async deleteProduct(@Param('id') id: string): Promise<void> {
+        await this.minimarketService.deleteProduct(+id);
+    }
+
+    @Get('categories')
+    async getCategories(): Promise<CategoryEntity[]> {
+        return await this.minimarketService.getCategories();
+    }
+
+    @Get('categories/:id')
+    async getCategory(@Param('id') id: string): Promise<CategoryEntity | null> {
+        return await this.minimarketService.getCategoryById(+id);
+    }
+
+    @Post('categories')
+    async createCategory(@Body() body: CategoryEntity): Promise<CategoryEntity> {
+        return await this.minimarketService.createCategory(body);
+    }
+
+    @Patch('categories/:id')
+    async updateCategory(@Param('id') id: string, @Body() body: CategoryEntity): Promise<CategoryEntity> {
+        return await this.minimarketService.updateCategory(+id, body);
+    }
+
+    @Delete('categories/:id')
+    async deleteCategory(@Param('id') id: string): Promise<void> {
+        await this.minimarketService.deleteCategory(+id);
+    }
+}
+
+
